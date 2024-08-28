39920aca46c3a22791e1d3772bd641b29798879d
futantan <luckytantanfu@gmail.com>
2024-08-28 21:04:25 +1000

    feat: 🎸 parse token id

--- packages/blinks/src/utils/handle-tokenscript.ts
+++ packages/blinks/src/utils/handle-tokenscript.ts
@@ -5,10 +5,14 @@ export const handleTokenScript = (actionUrl: URL) => {
     return actionUrl;
   }
 
+  // example:
+  // https://viewer.tokenscript.org/?chain=11155111&contract=0x31fc7840fA14F5e228E31D190b543deBDA13cCDA#card=Play&tokenId=28
+  // the tokenId is in the hash
   const params = new URLSearchParams(actionUrl.search);
+  const hashParams = new URLSearchParams(actionUrl.hash.slice(1));
   const chain = params.get('chain');
   const contract = params.get('contract');
-  const tokenId = params.get('tokenId');
+  const tokenId = params.get('tokenId') || hashParams.get('tokenId');
 
   if (chain && contract && tokenId) {
     return new URL(`http://localhost:3000/${chain}/${contract}/${tokenId}`);
