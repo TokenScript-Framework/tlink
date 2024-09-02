1ee11c5848cd9ee65dbb3651ecbde7ef94eb2500
futantan <luckytantanfu@gmail.com>
2024-09-02 16:22:05 +1000

    refactor: 💡 switch

--- apps/extension/src/background.ts
+++ apps/extension/src/background.ts
@@ -32,83 +32,87 @@ async function handleWalletCommunication(
   console.log('wallet', wallet);
   console.log('payload', payload);
 
-  //  case "eth_requestAccounts":
-  //  case "eth_sendTransaction":
-
-  // TODO:
-  //  case "eth_accounts":
-  //  case "eth_getCode":
-  //  case "eth_chainId":
-  //  case "net_version":
-  //  case "eth_blockNumber":
-  //  case "eth_estimateGas":
-  //  case "eth_getTransactionByHash":
-  //  case "eth_getTransactionReceipt":
-  //  case "eth_getTransactionCount":
-  //  case "personal_sign":
-  //  case "eth_call":
-  //  case "eth_signTypedData":
-  //  case "eth_signTypedData_v4":
-  //  case "eth_getBlockByNumber":
-  //  case "wallet_switchEthereumChain"
-
-  if (type === 'connect') {
-    console.log('connecting wallet', wallet);
-    const res = await chrome.scripting.executeScript({
-      world: 'MAIN',
-      target: { tabId: tabId },
-      func: async () => {
-        // @ts-ignore
-        const provider = window.ethereum;
-        const accounts = await provider.request({
-          method: 'eth_requestAccounts',
-        });
-        return accounts[0];
-      },
-    });
-    console.log('result', res);
-    return res[0].result;
-  } else if (type === 'sign_message') {
-    // // @ts-ignore
-    // console.log('signing message', payload.message);
-    // const res = await chrome.scripting.executeScript({
-    //   world: 'MAIN',
-    //   target: { tabId: tabId },
-    //   func: async (message: string) => {
-    //     const provider =
-    //       // @ts-ignore
-    //       wallet === 'solflare' ? window.solflare : window.solana;
-    //     const textToSign = new TextEncoder().encode(message);
-    //     const res = await provider.signMessage(textToSign);
-    //     return res;
-    //   },
-    //   // @ts-ignore
-    //   args: [payload.message, wallet],
-    // });
-    // return res[0].result;
-  } else if (type === 'eth_sendTransaction') {
-    // @ts-ignore
-    console.log('signing transaction', wallet, payload.txData);
-    const res = await chrome.scripting.executeScript({
-      world: 'MAIN',
-      target: { tabId: tabId },
-      func: async (transaction: any, wallet) => {
-        try {
+  switch (type) {
+    case 'connect':
+      console.log('connecting wallet', wallet);
+      const connectRes = await chrome.scripting.executeScript({
+        world: 'MAIN',
+        target: { tabId: tabId },
+        func: async () => {
           // @ts-ignore
           const provider = window.ethereum;
-          const res = await provider.request({
-            method: 'eth_sendTransaction',
-            params: [transaction],
+          const accounts = await provider.request({
+            method: 'eth_requestAccounts',
           });
-          return res;
-        } catch (e: any) {
-          console.log('error', e);
-          return { error: e.message ?? 'Unknown error' };
-        }
-      },
+          return accounts[0];
+        },
+      });
+      console.log('result', connectRes);
+      return connectRes[0].result;
+
+    case 'sign_message':
+      // // @ts-ignore
+      // console.log('signing message', payload.message);
+      // const signMessageRes = await chrome.scripting.executeScript({
+      //   world: 'MAIN',
+      //   target: { tabId: tabId },
+      //   func: async (message: string) => {
+      //     const provider =
+      //       // @ts-ignore
+      //       wallet === 'solflare' ? window.solflare : window.solana;
+      //     const textToSign = new TextEncoder().encode(message);
+      //     const res = await provider.signMessage(textToSign);
+      //     return res;
+      //   },
+      //   // @ts-ignore
+      //   args: [payload.message, wallet],
+      // });
+      // return signMessageRes[0].result;
+      break;
+
+    case 'eth_sendTransaction':
       // @ts-ignore
-      args: [payload.txData, wallet],
-    });
-    return res[0].result;
+      console.log('signing transaction', wallet, payload.txData);
+      const sendTransactionRes = await chrome.scripting.executeScript({
+        world: 'MAIN',
+        target: { tabId: tabId },
+        func: async (transaction: any, wallet) => {
+          try {
+            // @ts-ignore
+            const provider = window.ethereum;
+            const res = await provider.request({
+              method: 'eth_sendTransaction',
+              params: [transaction],
+            });
+            return res;
+          } catch (e: any) {
+            console.log('error', e);
+            return { error: e.message ?? 'Unknown error' };
+          }
+        },
+        // @ts-ignore
+        args: [payload.txData, wallet],
+      });
+      return sendTransactionRes[0].result;
+
+    default:
+      // TODO:
+      //  case "eth_accounts":
+      //  case "eth_getCode":
+      //  case "eth_chainId":
+      //  case "net_version":
+      //  case "eth_blockNumber":
+      //  case "eth_estimateGas":
+      //  case "eth_getTransactionByHash":
+      //  case "eth_getTransactionReceipt":
+      //  case "eth_getTransactionCount":
+      //  case "personal_sign":
+      //  case "eth_call":
+      //  case "eth_signTypedData":
+      //  case "eth_signTypedData_v4":
+      //  case "eth_getBlockByNumber":
+      //  case "wallet_switchEthereumChain"
+      console.log('Unknown type', type);
+      break;
   }
 }
