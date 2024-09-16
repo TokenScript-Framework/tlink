179ba3abf56fe144f5a6112287f1a9fce6f071f5
futantan <luckytantanfu@gmail.com>
2024-09-16 14:45:16 +1000

    feat: 🎸 add local

--- apps/tlink-web/app/tlink-registry/all/route.ts
+++ apps/tlink-web/app/tlink-registry/all/route.ts
@@ -4,18 +4,24 @@ export async function GET() {
   return Response.json(
     {
       actions: [
+        { host: 'localhost:3000', state: 'trusted' },
+        { host: 'localhost:3001', state: 'trusted' },
         { host: 'viewer.tokenscript.org', state: 'trusted' },
         { host: 'viewer-staging.tokenscript.org', state: 'trusted' },
         { host: 'www.charityconnect.io', state: 'trusted' },
         { host: 'testnet.charityconnect.io', state: 'trusted' },
       ],
       interstitials: [
+        { host: 'localhost:3000', state: 'trusted' },
+        { host: 'localhost:3001', state: 'trusted' },
         { host: 'viewer.tokenscript.org', state: 'trusted' },
         { host: 'viewer-staging.tokenscript.org', state: 'trusted' },
         { host: 'www.charityconnect.io', state: 'trusted' },
         { host: 'testnet.charityconnect.io', state: 'trusted' },
       ],
       websites: [
+        { host: 'localhost:3000', state: 'trusted' },
+        { host: 'localhost:3001', state: 'trusted' },
         { host: 'viewer.tokenscript.org', state: 'trusted' },
         { host: 'viewer-staging.tokenscript.org', state: 'trusted' },
         { host: 'www.charityconnect.io', state: 'trusted' },
