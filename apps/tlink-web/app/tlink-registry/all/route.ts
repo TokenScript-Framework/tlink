e54055f1c06e23d6fe9dd7c0360c4a6f6886c214
futantan <luckytantanfu@gmail.com>
2024-09-16 14:37:11 +1000

    feat: 🎸 whitelist

--- apps/tlink-web/app/tlink-registry/all/route.ts
+++ apps/tlink-web/app/tlink-registry/all/route.ts
@@ -3,8 +3,18 @@ import { ACTIONS_CORS_HEADERS } from '@repo/actions'
 export async function GET() {
   return Response.json(
     {
-      actions: [],
-      interstitials: [],
+      actions: [
+        { host: 'viewer.tokenscript.org', state: 'trusted' },
+        { host: 'viewer-staging.tokenscript.org', state: 'trusted' },
+        { host: 'www.charityconnect.io', state: 'trusted' },
+        { host: 'testnet.charityconnect.io', state: 'trusted' },
+      ],
+      interstitials: [
+        { host: 'viewer.tokenscript.org', state: 'trusted' },
+        { host: 'viewer-staging.tokenscript.org', state: 'trusted' },
+        { host: 'www.charityconnect.io', state: 'trusted' },
+        { host: 'testnet.charityconnect.io', state: 'trusted' },
+      ],
       websites: [
         { host: 'viewer.tokenscript.org', state: 'trusted' },
         { host: 'viewer-staging.tokenscript.org', state: 'trusted' },
