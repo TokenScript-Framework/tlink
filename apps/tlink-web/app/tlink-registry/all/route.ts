ae3b558fa173e5b690e1d668d48ab9512a6fc6f8
futantan <luckytantanfu@gmail.com>
2024-09-16 14:19:09 +1000

    feat: 🎸 tlink-registry api

---
+++ apps/tlink-web/app/tlink-registry/all/route.ts
@@ -0,0 +1,21 @@
+import { ACTIONS_CORS_HEADERS } from '@repo/actions'
+
+export async function GET() {
+  return Response.json(
+    {
+      actions: [],
+      interstitials: [],
+      websites: [
+        { host: 'viewer.tokenscript.org', state: 'trusted' },
+        { host: 'viewer-staging.tokenscript.org', state: 'trusted' },
+        { host: 'www.charityconnect.io', state: 'trusted' },
+        { host: 'testnet.charityconnect.io', state: 'trusted' },
+      ],
+    },
+    {
+      headers: ACTIONS_CORS_HEADERS,
+    },
+  )
+}
+
+export const OPTIONS = GET
