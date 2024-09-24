747636caaa2bb95ebf4e9a61b18de404389f7647
futantan <luckytantanfu@gmail.com>
2024-09-24 11:47:45 +1000

    feat: 🎸 show ts viewer

--- apps/tlink-extension/src/sandboxes/frame.tsx
+++ apps/tlink-extension/src/sandboxes/frame.tsx
@@ -8,7 +8,7 @@ function TestSandbox() {
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
       HELLO SANDBOX new
-      <iframe
+      {/* <iframe
         srcDoc={`
         <p>Hello world srcdoc!</p>
         <script>
@@ -19,6 +19,9 @@ function TestSandbox() {
           }, 2000);
         </script>
       `}>
+        <p>Your browser does not support iframes.</p>
+      </iframe> */}
+      <iframe src="https://viewer.tokenscript.org/?viewType=sts-token&chain=137&contract=0xd5ca946ac1c1f24eb26dae9e1a53ba6a02bd97fe&tokenId=1649017156&tokenscriptUrl=https%3A%2F%2Fviewer.tokenscript.org%2Fassets%2Ftokenscripts%2Fsmart-cat-prod.tsml&chainId=137">
         <p>Your browser does not support iframes.</p>
       </iframe>
     </div>
