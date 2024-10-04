537a117ef56685a6a040037e0de2f0524f87e477
futantan <luckytantanfu@gmail.com>
2024-10-04 11:46:51 +1000

    feat: 🎸 dialog styling

--- apps/tlink-extension/src/contents/ifram-popup.tsx
+++ apps/tlink-extension/src/contents/ifram-popup.tsx
@@ -1,10 +1,12 @@
 import cssText from "data-text:~style.css"
+import { CircleX, SquareArrowUpRight } from "lucide-react"
 import type { PlasmoCSConfig } from "plasmo"
 import { useEffect, useRef, useState } from "react"
 
-import "~style.css"
-
+import { Button } from "~components/button"
 import { Dialog, DialogContent } from "~components/dialog"
+
+import "~style.css"
 
 export const config: PlasmoCSConfig = {
   matches: ["https://twitter.com/*", "https://x.com/*", "https://pro.x.com/*"]
@@ -65,20 +67,43 @@ const PlasmoOverlay = () => {
     }
   }, [])
 
+  const dAppUrl =
+    "https://viewer.tokenscript.org/?viewType=sts-token&chain=137&contract=0xd5ca946ac1c1f24eb26dae9e1a53ba6a02bd97fe&tokenId=1649017156&chainId=137"
+
   return (
     <>
       <Dialog open={open} onOpenChange={setOpen}>
-        {/* <DialogTrigger asChild>
-          <button>hello</button>
-        </DialogTrigger> */}
-        <DialogContent className="sm:max-w-[425px] h-[800px] p-0 no-scrollbar">
-          <iframe
-            src={chrome.runtime.getURL("/sandboxes/frame.html")}
-            ref={iframeRef}
-            allow="clipboard-write"
-            sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-modals"
-            className="w-full h-full no-scrollbar"
-          />
+        <DialogContent className="sm:max-w-[425px] h-[800px] p-0 no-scrollbar rounded-lg">
+          <div className="rounded-lg overflow-hidden">
+            <div className="w-full h-12 px-2 flex justify-between items-center">
+              <Button
+                onClick={() => setOpen(false)}
+                variant="outline"
+                size="icon"
+                className="focus-visible:ring-0 focus-visible:ring-ring border-none shadow-none">
+                <CircleX className="h-6 w-6" />
+              </Button>
+
+              <div>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="focus-visible:ring-0 focus-visible:ring-ring border-none shadow-none">
+                  <SquareArrowUpRight className="h-6 w-6" />
+                </Button>
+              </div>
+            </div>
+
+            <iframe
+              src={chrome.runtime.getURL(
+                `/sandboxes/frame.html?url=${encodeURIComponent(dAppUrl)}`
+              )}
+              ref={iframeRef}
+              allow="clipboard-write"
+              sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-modals"
+              className="w-full h-full no-scrollbar rounded-lg"
+            />
+          </div>
         </DialogContent>
       </Dialog>
       <div className="z-50 flex fixed top-32 right-8 bg-gray-300 h-[800px]"></div>
--- apps/tlink-extension/src/sandboxes/frame.tsx
+++ apps/tlink-extension/src/sandboxes/frame.tsx
@@ -1,27 +1,24 @@
 import type { PlasmoCSConfig } from "plasmo"
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export const config: PlasmoCSConfig = {
   matches: ["https://twitter.com/*", "https://x.com/*", "https://pro.x.com/*"]
 }
 
-function TestSandbox() {
-  const chainId = 137
-  const contract = "0xd5ca946ac1c1f24eb26dae9e1a53ba6a02bd97fe"
-  const tokenId = "1649017156"
-
+function Sandbox() {
   const iframeRef = useRef<HTMLIFrameElement>(null)
+  const [dAppUrl, setDappUrl] = useState("")
 
-  const dAppUrl = `https://viewer.tokenscript.org/?viewType=sts-token&chain=${chainId}&contract=${contract}&tokenId=${tokenId}&chainId=${chainId}`
+  useEffect(() => {
+    const url = new URL(window.location.href)
+    const dAppUrl = url.searchParams.get("url")
+    setDappUrl(dAppUrl)
+  }, [])
 
   useEffect(() => {
     const handleMessage = async (event: MessageEvent) => {
       // The event.origin is null
-      // console.log("tlink messaging 11111111111111111", {
-      //   event,
-      //   "window-ethereum": window.ethereum,
-      //   "chrome-runtime": chrome.runtime
-      // })
+      // console.log("tlink messaging 11111111111111111", { event, })
       // here we can't use chrome.runtime.sendMessage, it's undefined
       // window.ethereum is also undefined
       if (event.data.source === "tlink-rpc-resp") {
@@ -32,25 +29,47 @@ function TestSandbox() {
         window.parent.postMessage({ source: "tlink", data: event.data }, "*")
       }
     }
-
     window.addEventListener("message", handleMessage)
 
     return () => window.removeEventListener("message", handleMessage)
   }, [])
 
+  useGlobalStyle()
+
   return (
     <div style={{ height: "800px" }}>
-      <iframe
-        // key={`${chainId}-${contract}-${tokenId}-${address}`}
-        ref={iframeRef}
-        style={{ height: "100%", width: "100%" }}
-        src={dAppUrl}
-        className="relative size-full"
-        allow="clipboard-write"
-        sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-modals"
-      />
+      {dAppUrl && (
+        <iframe
+          key={dAppUrl}
+          ref={iframeRef}
+          style={{ height: "100%", width: "100%", borderWidth: 0 }}
+          src={dAppUrl}
+          className="relative size-full"
+          allow="clipboard-write"
+          sandbox="allow-same-origin allow-scripts allow-forms allow-popups allow-modals"
+        />
+      )}
     </div>
   )
 }
 
-export default TestSandbox
+export default Sandbox
+
+function useGlobalStyle() {
+  useEffect(() => {
+    // Add global CSS
+    const style = document.createElement("style")
+    style.textContent = `
+      body {
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+      }
+    `
+    document.head.appendChild(style)
+
+    return () => {
+      document.head.removeChild(style)
+    }
+  }, [])
+}
