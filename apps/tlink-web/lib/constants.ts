c3b031b7522fd042bbc9ee10aa713fb6d6a7c0c8
futantan <luckytantanfu@gmail.com>
2024-11-06 11:20:04 +1100

    feat: 🎸 landing page

--- apps/tlink-web/lib/constants.ts
+++ apps/tlink-web/lib/constants.ts
@@ -1,15 +1,24 @@
 export const tlinks = [
   {
     // Tantan Fu's smart cat
+    twitter: 'https://x.com/TantanFu122765/status/1828692495565479948',
     link: 'https://viewer.tokenscript.org/?chain=137&contract=0xd5ca946ac1c1f24eb26dae9e1a53ba6a02bd97fe&tokenId=1649017156#',
   },
   {
+    twitter: 'https://x.com/TantanFu122765/status/1846716253416968590',
     // Mooarty
     link: 'https://viewer.tokenscript.org/?chain=11155111&contract=0x31fc7840fA14F5e228E31D190b543deBDA13cCDA#card=Play&tokenId=28',
   },
-  { link: 'https://buy-me-a-beer-sigma.vercel.app/TantanFu' },
-  { link: 'https://www.charityconnect.io/' },
   {
+    twitter: 'https://x.com/TantanFu122765/status/1841370375982346612',
+    link: 'https://buy-me-a-beer-sigma.vercel.app/TantanFu',
+  },
+  {
+    twitter: 'https://x.com/TantanFu122765/status/1836231447121924370',
+    link: 'https://www.charityconnect.io/',
+  },
+  {
+    twitter: 'https://x.com/TantanFu122765/status/1853953929484378276',
     // by me a beer, Jia's link
     link: 'https://viewer.tokenscript.org/?chain=8453&contract=0x80a6da00140c4798baba3b3f362839b6f87b6fc6&tokenId=21',
   },
